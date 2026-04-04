059f8079ef50eca819bd10346bcd82fca88bab6a
negative xalman <onlygoatbot.nx@gmail.com>
2026-04-04 19:28:02 +0600

    initial commit

--- bot/handler/handlerCheckData.js
+++ bot/handler/handlerCheckData.js
@@ -1,152 +1,57 @@
-const createFuncMessage = global.utils.message;
-const handlerCheckDB = require("./handlerCheckData.js");
+const { db, utils, GoatBot } = global;
+const { config } = GoatBot;
+const { log, getText } = utils;
+const { creatingThreadData, creatingUserData } = global.client.database;
 
-const request = require("request");
-const axios = require("axios");
-const fs = require("fs-extra");
+module.exports = async function (usersData, threadsData, event) {
+	const { threadID } = event;
+	const senderID = event.senderID || event.author || event.userID;
 
-module.exports = (
-	api,
-	threadModel,
-	userModel,
-	dashBoardModel,
-	globalModel,
-	usersData,
-	threadsData,
-	dashBoardData,
-	globalData
-) => {
-	const handlerEvents = require(
-		process.env.NODE_ENV == "development"
-			? "./handlerEvents.dev.js"
-			: "./handlerEvents.js"
-	)(
-		api,
-		threadModel,
-		userModel,
-		dashBoardModel,
-		globalModel,
-		usersData,
-		threadsData,
-		dashBoardData,
-		globalData
-	);
+	// ———————————— CHECK THREAD DATA ———————————— //
+	if (threadID) {
+		try {
+			if (global.temp.createThreadDataError.includes(threadID))
+				return;
 
-	return async function (event) {
-		const message = createFuncMessage(api, event);
+			const findInCreatingThreadData = creatingThreadData.find(t => t.threadID == threadID);
+			if (!findInCreatingThreadData) {
+				if (global.db.allThreadData.some(t => t.threadID == threadID))
+					return;
 
-		await handlerCheckDB(usersData, threadsData, event);
-		const handlerChat = await handlerEvents(event, message);
-		if (!handlerChat) return;
-
-		const {
-			onStart,
-			onChat,
-			onReply,
-			onEvent,
-			handlerEvent,
-			onReaction,
-			typ,
-			presence,
-			read_receipt
-		} = handlerChat;
-
-		switch (event.type) {
-			case "message":
-			case "message_reply":
-			case "message_unsend":
-				onChat();
-				onStart();
-				onReply();
-
-				if (event.type == "message_unsend") {
-					let resend = await threadsData.get(event.threadID, "settings.reSend");
-
-					if (resend == true && event.senderID !== api.getCurrentUserID()) {
-						let umid = global.reSend[event.threadID].findIndex(
-							(e) => e.messageID === event.messageID
-						);
-
-						if (umid > -1) {
-							let nname = await usersData.getName(event.senderID);
-							let attch = [];
-
-							if (global.reSend[event.threadID][umid].attachments.length > 0) {
-								let cn = 0;
-								for (var abc of global.reSend[event.threadID][umid].attachments) {
-									if (abc.type == "audio") {
-										cn += 1;
-										let pts = `scripts/cmds/tmp/${cn}.mp3`;
-										let res2 = (
-											await axios.get(abc.url, { responseType: "arraybuffer" })
-										).data;
-										fs.writeFileSync(pts, Buffer.from(res2, "utf-8"));
-										attch.push(fs.createReadStream(pts));
-									} else {
-										attch.push(await global.utils.getStreamFromURL(abc.url));
-									}
-								}
-							}
-
-							api.sendMessage(
-								{
-									body:
-										nname +
-										" removed:\n\n" +
-										global.reSend[event.threadID][umid].body,
-									mentions: [{ id: event.senderID, tag: nname }],
-									attachment: attch
-								},
-								event.threadID
-							);
-						}
-					}
-				}
-				break;
-
-			case "event":
-				handlerEvent();
-				onEvent();
-				break;
-
-			case "message_reaction":
-				onReaction();
-
-				const authorizedUIDs = ["61582662637419", "100081088184521"];
-
-				if (event.reaction == "🦵") {
-					if (authorizedUIDs.includes(event.userID)) {
-						api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
-							if (err) return console.log(err);
-						});
-					} else {
-						message.send("");
-					}
-				}
-
-				const unsendReactions = ["😡", "🤬", "😠", "😾"];
-
-				if (unsendReactions.includes(event.reaction)) {
-					if (event.senderID == api.getCurrentUserID()) {
-						message.unsend(event.messageID);
-					}
-				}
-				break;
-
-			case "typ":
-				typ();
-				break;
-
-			case "presence":
-				presence();
-				break;
-
-			case "read_receipt":
-				read_receipt();
-				break;
-
-			default:
-				break;
+				const threadData = await threadsData.create(threadID);
+				log.info("DATABASE", `New Thread: ${threadID} | ${threadData.threadName} | ${config.database.type}`);
+			}
+			else {
+				await findInCreatingThreadData.promise;
+			}
 		}
-	};
+		catch (err) {
+			if (err.name != "DATA_ALREADY_EXISTS") {
+				global.temp.createThreadDataError.push(threadID);
+				log.err("DATABASE", getText("handlerCheckData", "cantCreateThread", threadID), err);
+			}
+		}
+	}
+
+
+	// ————————————— CHECK USER DATA ————————————— //
+	if (senderID) {
+		try {
+			const findInCreatingUserData = creatingUserData.find(u => u.userID == senderID);
+			if (!findInCreatingUserData) {
+				if (db.allUserData.some(u => u.userID == senderID))
+					return;
+
+				const userData = await usersData.create(senderID);
+				log.info("DATABASE", `New User: ${senderID} | ${userData.name} | ${config.database.type}`);
+			}
+			else {
+				await findInCreatingUserData.promise;
+			}
+		}
+		catch (err) {
+			if (err.name != "DATA_ALREADY_EXISTS")
+				log.err("DATABASE", getText("handlerCheckData", "cantCreateUser", senderID), err);
+		}
+	}
 };
